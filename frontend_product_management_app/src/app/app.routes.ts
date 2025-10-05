8b458007b4130a4401c24c9a526f043e2a417382
Yefry Mendoza <ymendoza@citylendinginc.com>
2025-10-05 13:54:30 -0500

    Integrando main layout

--- frontend_product_management_app/src/app/app.routes.ts
+++ frontend_product_management_app/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from './layout/layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  {
+    path: 'app',
+    component: LayoutComponent,
+    // canActivate: [authGuard],
+    children: [
+      // { path: '', pathMatch: 'full', redirectTo: 'blank' }
+    ]
+  },
+
+  { path: '**', redirectTo: 'app' }
+];
---
+++ frontend_product_management_app/src/app/layout/layout.component.ts
@@ -0,0 +1,30 @@
+import { Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'app-layout',
+  imports: [
+    CommonModule,
+    RouterModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatListModule,
+    MatButtonModule
+  ],
+  templateUrl: './layout.component.html',
+  styleUrl: './layout.component.scss'
+})
+export class LayoutComponent {
+  sidebarOpen = signal(true);
+
+  toggleSidebar() {
+    this.sidebarOpen.update(v => !v);
+  }
+}
